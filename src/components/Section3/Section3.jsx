564e7ace1416abe37412d65423a09a581b95abbb
ezequielg1989 <ezequiel.ganzero1@gmail.com>
2022-04-21 12:39:29 -0300

    port

--- src/components/Section3/Section3.jsx
+++ src/components/Section3/Section3.jsx
@@ -9,7 +9,7 @@ export const Section3 = () => {
             <div className="col-10 col-lg-5 col-md-4 col-sm-10 box">
                 <div className="container col text-center">
                     <div className="row d-flex ">
-                        <img className='img-port mt-3' src="../public/img/Captura de pantalla de 2022-04-20 18-04-20.jpg" alt="" />
+                        <img className='img-port mt-3' src="https://firebasestorage.googleapis.com/v0/b/mobile-react-app.appspot.com/o/Captura%20de%20pantalla%20de%202022-04-20%2018-04-20.png?alt=media&token=280e0c8a-3092-4568-b4b9-b3188ff9260d" alt="" />
                         <div className="container">
                             <div className="row d-flex col mt-3 justify-content-center">
                                 <span className='text-exp'>MobileStore</span>
@@ -24,7 +24,7 @@ export const Section3 = () => {
             <div className="col-10 col-lg-5 col-md-4 col-sm-10  box">
                 <div className="container col text-center">
                     <div className="row d-flex ">
-                        <img className='img-port mt-3' src="public/Captura de pantalla de 2022-04-20 18-04-20.jpg" alt="" />
+                        <img className='img-port mt-3' src="https://firebasestorage.googleapis.com/v0/b/mobile-react-app.appspot.com/o/Captura%20de%20pantalla%20de%202022-04-20%2019-18-48.png?alt=media&token=df4b3dad-e3a4-467e-86e8-2bb8968b1bce" alt="" />
                         <div className="container">
                             <div className="row d-flex col mt-3 justify-content-center">
                                 <span className='text-exp'>.DANCCA</span>
@@ -38,7 +38,7 @@ export const Section3 = () => {
             <div className="col-10 col-lg-5 col-md-4 col-sm-10 box">
                 <div className="container col text-center">
                     <div className="row d-flex ">
-                        <img className='img-port mt-3' src="public/Captura de pantalla de 2022-04-20 19-18-48.jpg" alt="" />
+                        <img className='img-port mt-3' src="https://firebasestorage.googleapis.com/v0/b/mobile-react-app.appspot.com/o/Captura%20de%20pantalla%20de%202022-04-20%2018-04-20.png?alt=media&token=280e0c8a-3092-4568-b4b9-b3188ff9260d" alt="" />
                         <div className="container">
                             <div className="row d-flex col mt-3 justify-content-center">
                                 <span className='text-exp'>MobileStore</span>
