7e21b4ba1fa3dbf017b62452290979c134b23d7d
ezequielg1989 <ezequiel.ganzero1@gmail.com>
2022-04-22 12:14:53 -0300

    bootstrap1

--- src/components/Section3/Section3.jsx
+++ src/components/Section3/Section3.jsx
@@ -11,11 +11,10 @@ export const Section3 = () => {
                     <div className="row d-flex ">
                         <img className='img-port mt-3' src="https://firebasestorage.googleapis.com/v0/b/mobile-react-app.appspot.com/o/Captura%20de%20pantalla%20de%202022-04-20%2018-04-20.png?alt=media&token=280e0c8a-3092-4568-b4b9-b3188ff9260d" alt="" />
                         <div className="container">
-                            <div className="row d-flex col mt-3 justify-content-center">
-                                <span className='text-exp'>MobileStore</span>
-                                <div className="btn btn-primary  col col-sm-4 me-3 mt-3"><a href="https://github.com/ezequielg1989/mobile" target="_blank">GitHub</a></div>
-                                <div className="btn btn-primary  col col-sm-4 me-3 mt-3"><a href="https://mobiless-store.netlify.app/" target="_blank">Demo</a></div>
-                                
+                            <div className="row d-flex col  justify-content-center">
+                                <span className='text-exp mt-4 mb-4'>MobileStore</span>
+                                <div className="btn btn-primary  col col-sm-4 me-3 mt-2 p-2"><a href="https://github.com/ezequielg1989/mobile" target="_blank">GitHub</a></div>
+                                <div className="btn btn-primary  col col-sm-4 me-3 mt-2 p-2"><a href="https://mobiless-store.netlify.app/" target="_blank">Demo</a></div>
                             </div>
                         </div>
                     </div>
@@ -26,10 +25,10 @@ export const Section3 = () => {
                     <div className="row d-flex ">
                         <img className='img-port mt-3' src="https://firebasestorage.googleapis.com/v0/b/mobile-react-app.appspot.com/o/Captura%20de%20pantalla%20de%202022-04-20%2019-18-48.png?alt=media&token=df4b3dad-e3a4-467e-86e8-2bb8968b1bce" alt="" />
                         <div className="container">
-                            <div className="row d-flex col mt-3 justify-content-center">
-                                <span className='text-exp'>.DANCCA</span>
-                                <div className="btn btn-primary  col col-sm-4 me-3 mt-3"><a href="https://github.com/ezequielg1989/tienda-ecommerce" target="_blank">GitHub</a></div>
-                                <div className="btn btn-primary  col col-sm-4 me-3 mt-3"><a href="https://dancca.netlify.app/" target="_blank">Demo</a></div>
+                            <div className="row d-flex col  justify-content-center">
+                                <span className='text-exp mt-4 mb-4'>.DANCCA</span>
+                                <div className="btn btn-primary  col col-sm-4 me-3 mt-2 p-2"><a href="https://github.com/ezequielg1989/tienda-ecommerce" target="_blank">GitHub</a></div>
+                                <div className="btn btn-primary  col col-sm-4 me-3 mt-2 p-2"><a href="https://dancca.netlify.app/" target="_blank">Demo</a></div>
                             </div>
                         </div>
                     </div>
@@ -40,10 +39,10 @@ export const Section3 = () => {
                     <div className="row d-flex ">
                         <img className='img-port mt-3' src="https://firebasestorage.googleapis.com/v0/b/mobile-react-app.appspot.com/o/Captura%20de%20pantalla%20de%202022-04-21%2020-19-28.png?alt=media&token=1003a493-10c1-4625-9c56-d3d745de9174" alt="" />
                         <div className="container">
-                            <div className="row d-flex col mt-3 justify-content-center">
-                                <span className='text-exp'>MobileStore</span>
-                                <div className="btn btn-primary  col col-sm-4 me-3 mt-3"><a href="https://github.com/ezequielg1989/mobile" target="_blank">GitHub</a></div>
-                                <div className="btn btn-primary  col col-sm-4 me-3 mt-3"><a href="https://nccinema.netlify.app/" target="_blank">Demo</a></div>
+                            <div className="row d-flex col justify-content-center">
+                                <span className='text-exp mt-4 mb-4'>MobileStore</span>
+                                <div className="btn btn-primary  col col-sm-4 me-3 mt-2 p-2"><a href="https://github.com/ezequielg1989/mobile" target="_blank">GitHub</a></div>
+                                <div className="btn btn-primary  col col-sm-4 me-3 mt-2 p-2"><a href="https://nccinema.netlify.app/" target="_blank">Demo</a></div>
                                 
                             </div>
                         </div>
